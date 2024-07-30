92ad43ddb077f4b21af0c3cc8c34f3a691923a08
helloakg <helloakshayg@gmail.com>
2024-07-30 12:26:10 +0530

    added editContact Test

--- com.mavenCmdDemo/src/test/java/module2/contactModuleTest.java
+++ com.mavenCmdDemo/src/test/java/module2/contactModuleTest.java
@@ -24,7 +24,7 @@ public class contactModuleTest {
 	}
 
 	@Test(groups={"regression"})
-	public void DeleteOrgTest() {
+	public void deleteContactTest() {
 		String BROWSER_NAME = System.getProperty("browser","default_browser");
 		String URL = System.getProperty("url","default_url");
 		String USERNAME = System.getProperty("username","default_user");
@@ -37,6 +37,24 @@ public class contactModuleTest {
 		System.out.println("--Delete Contact---");
 		System.out.println("--Close Browser---");
 		System.out.println("--Ending delete contact---\n");
+		//Assert.fail();
+
+	}
+	
+	@Test(groups={"regression"})
+	public void editContactTest() {
+		String BROWSER_NAME = System.getProperty("browser","default_browser");
+		String URL = System.getProperty("url","default_url");
+		String USERNAME = System.getProperty("username","default_user");
+		String PASSWORD = System.getProperty("password","default_password");
+		
+		System.out.println(BROWSER_NAME+"_"+URL+"_"+USERNAME+"_"+PASSWORD);
+		
+		System.out.println("--Regression: Executing edit contact---");
+		System.out.println("--Open Browser---");
+		System.out.println("--Edit Contact---");
+		System.out.println("--Close Browser---");
+		System.out.println("--Ending edit contact---\n");
 		//Assert.fail();
 
 	}
